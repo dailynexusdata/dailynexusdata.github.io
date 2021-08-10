d49ca98abb105f0b919c3074da03e33b242a0860
alrudolph <alex3r@me.com>
2021-08-10 12:24:49 -0700

    header image

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import { Link } from "gatsby";
 
+import LabyrinthHeader from "../../images/headerLogo.png";
+
 const Header = () => {
   return (
     <>
+      <img src={LabyrinthHeader} />
       <Link to="../guides">Guides</Link>
     </>
   );
